1c51969b036bc00b132b6fb1917899f0188e9431
MARCO LOPEZ <mlopez@marostdevelopers.com>
2014-03-04 22:16:49 -0500

    Home: script para base de url

--- wg-header-script.php
+++ wg-header-script.php
@@ -1,3 +1,4 @@
+<base href="<?php echo $web; ?>"></base>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="description" content="Responsive Minimal Bootstrap Theme">
 <meta name="keywords" content="responsive,minimal,bootstrap,theme">
