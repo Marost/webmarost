f36ef0f61b00912742abe5ec306574678c7a4b37
MARCO LOPEZ <mlopez@marostdevelopers.com>
2014-03-04 22:17:07 -0500

    Home: lista de noticias en blog

--- blog.php
+++ blog.php
@@ -1,3 +1,10 @@
+<?php
+require_once("panel@marost/conexion/conexion.php");
+require_once("panel@marost/conexion/funciones.php");
+
+//NOTICIAS
+$rst_notas=mysql_query("SELECT * FROM mrt_noticia WHERE publicar=1 ORDER BY fecha_publicacion DESC", $conexion);
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -19,12 +26,11 @@
             <div class="container">
                 <div class="row">
                     <div class="span12">
-                        <h1>Blog Full</h1>
-                        <span>Some Cool Page Description.</span>
+                        <h1>Blog</h1>
                         <ul class="crumb">
-                            <li><a href="index.html">Home</a></li>
+                            <li><a href="/">Inicio</a></li>
                             <li class="sep">/</li>
-                            <li>Blog Full</li>
+                            <li>Blog</li>
                         </ul>
                     </div>
                 </div>
@@ -38,17 +44,29 @@
                 <div class="row">
                     <div class="span12">
                         <ul class="blog-list">
+
+                            <?php while($fila_nota=mysql_fetch_array($rst_notas)){
+                                    $nota_id=$fila_nota["id"];
+                                    $nota_url=$fila_nota["url"];
+                                    $nota_titulo=$fila_nota["titulo"];
+                                    $nota_contenido=primerParrafo($fila_nota["contenido"]);
+                                    $nota_imagen=$fila_nota["imagen"];
+                                    $nota_imagen_carpeta=$fila_nota["imagen_carpeta"];
+                                    $nota_fecha_publicacion=$fila_nota["fecha_publicacion"];
+                                    $nota_tags=$fila_nota["tags"];
+
+                                    //URLS
+                                    $nota_URLWeb=$web."blog/".$nota_id."-".$nota_url;
+                                    $nota_URLImg=$web."imagenes/upload/".$nota_imagen_carpeta."".$nota_imagen;
+                            ?>
                             <li>
-                                <div class="date-box"><span class="day">20</span> <span class="month">SEP</span> </div>
+                                <div class="date-box"><span class="day">20</span> <span class="month">SEP</span></div>
                                 <div class="post-content">
                                     <div class="post-image">
                                         <div class="callbacks_container">
                                             <ul class="rslides pic_slider">
                                                 <li>
-                                                    <img src="imagenes/pic-blog-1.jpg" alt="" />
-                                                </li>
-                                                <li>
-                                                    <img src="imagenes/pic-blog-2.jpg" alt="" />
+                                                    <img src="<?php echo $nota_URLImg; ?>" alt="" />
                                                 </li>
                                             </ul>
                                         </div>
@@ -56,52 +74,13 @@
 
 
                                     <div class="post-text">
-                                        <h3><a href="css/#">Sed ut perspiciatis unde omnis iste natus</a></h3>
-                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum [...]
+                                        <h3><a href="<?php echo $nota_URLWeb; ?>"><?php echo $nota_titulo; ?></a></h3>
+                                        <?php echo $nota_contenido; ?>
                                     </div>
                                 </div>
-                                <div class="post-meta"><span><i class="icon-user"></i>By: <a href="#">Lynda Wu</a></span> <span><i class="icon-tag"></i><a href="#">News</a>, <a href="#">Events</a></span> <span><i class="icon-comment"></i><a href="#">10 Comments</a></span> <span><i class="icon-arrow-right"></i><a href="#">Read More</a></span> </div>
                             </li>
+                            <?php } ?>
 
-                            <li>
-                                <div class="date-box"><span class="day">20</span> <span class="month">SEP</span> </div>
-                                <div class="post-content">
-                                    <div class="post-image">
-                                        <span class="post-text">
-                                            <img src="imagenes/pic-blog-3.jpg" alt=""></span>
-                                    </div>
-                                    <div class="post-text">
-                                        <h3><a href="#">Sed ut perspiciatis unde omnis iste natus</a></h3>
-                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum [...]
-                                    </div>
-                                </div>
-                                <div class="post-meta"><span><i class="icon-user"></i>By: <a href="#">Lynda Wu</a></span> <span><i class="icon-tag"></i><a href="#">News</a>, <a href="#">Events</a></span> <span><i class="icon-comment"></i><a href="#">10 Comments</a></span> <span><i class="icon-arrow-right"></i><a href="#">Read More</a></span> </div>
-                            </li>
-
-                            <li>
-                                <div class="date-box"><span class="day">20</span> <span class="month">SEP</span> </div>
-                                <div class="post-content">
-                                    <div class="post-image">
-                                        <div class="callbacks_container">
-                                            <ul class="rslides pic_slider">
-                                                <li>
-                                                    <img src="imagenes/pic-blog-4.jpg" alt="" />
-                                                </li>
-                                                <li>
-                                                    <img src="imagenes/pic-blog-5.jpg" alt="" />
-                                                </li>
-                                            </ul>
-                                        </div>
-                                    </div>
-
-
-                                    <div class="post-text">
-                                        <h3><a href="css/#">Sed ut perspiciatis unde omnis iste natus</a></h3>
-                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum [...]
-                                    </div>
-                                </div>
-                                <div class="post-meta"><span><i class="icon-user"></i>By: <a href="#">Lynda Wu</a></span> <span><i class="icon-tag"></i><a href="#">News</a>, <a href="#">Events</a></span> <span><i class="icon-comment"></i><a href="#">10 Comments</a></span> <span><i class="icon-arrow-right"></i><a href="#">Read More</a></span> </div>
-                            </li>
                         </ul>
 
                         <div class="pagination text-center ">
