7d2ff927173cba23ea0a2c3d45ba807859a11795
Marco Lopez <mlopez@virtualsip.net>
2014-03-07 16:01:57 -0500

    Administrador: agregando opciones(social y urladmin) en conexion

--- panel@marost/conexion/conexion.php
+++ panel@marost/conexion/conexion.php
@@ -18,7 +18,17 @@ global $usuario_nombre;
 global $usuario_apellido;
 global $usuario_email;
 global $web;
+global $web_nombre;
+global $web_nosotros;
 global $fechaActual;
+
+//SOCIAL
+global $social_email;
+global $social_facebook;
+global $social_twitter;
+global $social_youtube;
+global $social_palabrasclave;
+global $social_nosotros;
 
 //VARIABLES
 $carpeta_admin="panel@marost";
@@ -30,11 +40,25 @@ $fechaActual=date("Y-m-d H:i:s");
 $rst_empresa=mysql_query("SELECT * FROM ".$tabla_suf."_empresa WHERE id=1;", $conexion);
 $fila_empresa=mysql_fetch_array($rst_empresa);
 $web=$fila_empresa["web"];
+$web_nombre=$fila_empresa["nombre"];
+$web_nosotros=$fila_empresa["nosotros"];
+
+//SOCIAL
+$social_email=$fila_empresa["social_email"];
+$social_facebook=$fila_empresa["social_facebook"];
+$social_twitter=$fila_empresa["social_twitter"];
+$social_youtube=$fila_empresa["social_youtube"];
+$social_youtube=$fila_empresa["social_youtube"];
+$social_palabrasclave=$fila_empresa["palabras_clave"];
+$social_nosotros=$fila_empresa["nosotros"];
+
+//URL DE ARCHIVOS
+$url_admin=$web."".$carpeta_admin."/";
 
 if ($_SESSION["user-".$sesion_pre.""]<>""){
 	$usuario_user=$_SESSION["user-".$sesion_pre.""];
 	$usuario_nombre=$_SESSION["user_nombre-".$sesion_pre.""];
 	$usuario_apellido=$_SESSION["user_apellido-".$sesion_pre.""];
-	$usuario_email=$_SESSION["user_email-".$sesion_pre.""];	
+	$usuario_email=$_SESSION["user_email-".$sesion_pre.""];
 }
 ?>
