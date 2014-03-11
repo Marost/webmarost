e063ed45728d38395c538f415252a03f1c202185
MARCO LOPEZ <mlopez@marostdevelopers.com>
2014-03-10 22:33:18 -0500

    Home: enlace de menu

--- wg-header.php
+++ wg-header.php
@@ -2,22 +2,22 @@
     <div class="container">
         <div id="logo">
             <div class="inner">
-                <a href="index.php">
+                <a href="/">
                     <img src="imagenes/logo.png" alt="logo"></a>
             </div>
         </div>
 
         <!-- mainmenu begin -->
         <ul id="mainmenu">
-            <li><a href="index.php">Inicio</a></li>
+            <li><a href="/">Inicio</a></li>
             <li><a href="#">Servicios</a>
             	<ul>
                 	<li><a href="packages.html">Hosting</a></li>
                 	<li><a href="services.html">Diseño de Páginas Web</a></li>
                 </ul>
             </li>
-            <li><a href="#">Portafolio</a></li>
-            <li><a href="blog.html">Blog</a></li>
+            <li><a href="portafolio">Portafolio</a></li>
+            <li><a href="blog">Blog</a></li>
             <li><a href="contacto.php">Contacto</a></li>
         </ul>
         <!-- mainmenu close -->
