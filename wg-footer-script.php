0a648ec63a2325fc66f0dc3cc304509c3903484b
MARCO LOPEZ <mlopez@marostdevelopers.com>
2014-03-31 08:21:16 -0500

    Home: modificacion para mostrar script en portafolio y home

--- wg-footer-script.php
+++ wg-footer-script.php
@@ -2,16 +2,22 @@
 ================================================== -->
 <script src="js/jquery.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
-<script src="js/jquery.isotope.min.js"></script>
 <script src="js/jquery.prettyPhoto.js"></script>
 <script src="js/easing.js"></script>
 <script src="js/jquery.ui.totop.js"></script>
 <script src="js/selectnav.js"></script>
 <script src="js/ender.js"></script>
 <script src="js/jquery.lazyload.js"></script>
-<script src="rs-plugin/js/jquery.themepunch.revolution.min.js" type="text/javascript"></script>
+
 <script src="js/jquery.flexslider-min.js"></script>
 <script src="js/custom.js"></script>
+
+<script src="js/jquery.isotope.min.js"></script>
+
+<?php if($script_themepunch==true){ ?>
+<!-- THEMEPUNCH -->
+<script src="rs-plugin/js/jquery.themepunch.revolution.min.js" type="text/javascript"></script>
+<?php } ?>
 
 <!-- Javascript Files for current page only -->
 <script src="js/rev-setting-1.js"></script>
