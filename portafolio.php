13d1d6d732b1fb14f5ef678bb8c1881f673062e4
MARCO LOPEZ <mlopez@marostdevelopers.com>
2014-03-31 08:22:20 -0500

    Home: mostrar lista de portafolio

--- portafolio.php
+++ portafolio.php
@@ -1,6 +1,9 @@
 <?php
 require_once("panel@marost/conexion/conexion.php");
 require_once("panel@marost/conexion/funciones.php");
+
+//PORTAFOLIO
+$rst_porta=mysql_query("SELECT * FROM mrt_portafolio WHERE publicar=1 ORDER BY titulo ASC; ", $conexion);
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -43,221 +46,34 @@ require_once("panel@marost/conexion/funciones.php");
                 <div class="row">
                     <div id="gallery" class="gallery">
 
-                        <!-- gallery item -->
-                        <div class="span4  item">
-                            <div class="picframe">
-                                <span class="overlay">
-                                    <span class="info-area">
-                                        <a class="img-icon-url" href="project-single.html"></a>
-                                        <a class="img-icon-zoom" href="imagenes/portfolio/pf%20(1).jpg" data-type="prettyPhoto[gallery]" title=""></a>
-                                    </span>
-                                    <span class="pf_text">
-                                        <span class="project-name">Project Name #1</span>
-                                        <span>Mobile</span>
-                                    </span>
-                                </span>
-                                <img src="imagenes/pf_blank.gif" data-original="imagenes/portfolio/pf%20(1).jpg" alt="" />
-                            </div>
-                        </div>
-                        <!-- close gallery item -->
+                        <?php while($fila_porta=mysql_fetch_array($rst_porta)){
+                                $porta_url=$fila_porta["url"];
+                                $porta_titulo=$fila_porta["titulo"];
+                                $porta_enlace=$fila_porta["enlace"];
+                                $porta_imagen=$fila_porta["imagen"];
+                                $porta_imagen_carpeta=$fila_porta["imagen_carpeta"];
 
+                                //URLS
+                                $porta_UrlWeb=$web."portafolio/".$porta_url;
+                                $porta_UrlImg=$web."imagenes/upload/".$porta_imagen_carpeta."".$porta_imagen;
+                        ?>
                         <!-- gallery item -->
                         <div class="span4  item">
                             <div class="picframe">
                                 <span class="overlay">
                                     <span class="info-area">
-                                        <a class="img-icon-url" href="project-single.html"></a>
-                                        <a class="img-icon-zoom" href="imagenes/portfolio/pf%20(1).jpg" data-type="prettyPhoto[gallery]" title=""></a>
+                                        <a class="img-icon-url" href="<?php echo $porta_UrlWeb; ?>"></a>
+                                        <a class="img-icon-zoom" href="<?php echo $porta_UrlImg; ?>" data-type="prettyPhoto[gallery]" title=""></a>
                                     </span>
                                     <span class="pf_text">
-                                        <span class="project-name">Project Name #1</span>
-                                        <span>Mobile</span>
+                                        <span class="project-name"><?php echo $porta_titulo; ?></span>
                                     </span>
                                 </span>
-                                <img src="imagenes/pf_blank.gif" data-original="imagenes/portfolio/pf%20(1).jpg" alt="" />
+                                <img src="<?php echo $porta_UrlImg; ?>" data-original="<?php echo $porta_UrlImg; ?>" alt="" />
                             </div>
                         </div>
                         <!-- close gallery item -->
-
-                        <!-- gallery item -->
-                        <div class="span4  item">
-                            <div class="picframe">
-                                <span class="overlay">
-                                    <span class="info-area">
-                                        <a class="img-icon-url" href="project-single.html"></a>
-                                        <a class="img-icon-zoom" href="imagenes/portfolio/pf%20(1).jpg" data-type="prettyPhoto[gallery]" title=""></a>
-                                    </span>
-                                    <span class="pf_text">
-                                        <span class="project-name">Project Name #1</span>
-                                        <span>Mobile</span>
-                                    </span>
-                                </span>
-                                <img src="imagenes/pf_blank.gif" data-original="imagenes/portfolio/pf%20(1).jpg" alt="" />
-                            </div>
-                        </div>
-                        <!-- close gallery item -->
-
-                        <!-- gallery item -->
-                        <div class="span4  item">
-                            <div class="picframe">
-                                <span class="overlay">
-                                    <span class="info-area">
-                                        <a class="img-icon-url" href="project-single.html"></a>
-                                        <a class="img-icon-zoom" href="imagenes/portfolio/pf%20(1).jpg" data-type="prettyPhoto[gallery]" title=""></a>
-                                    </span>
-                                    <span class="pf_text">
-                                        <span class="project-name">Project Name #1</span>
-                                        <span>Mobile</span>
-                                    </span>
-                                </span>
-                                <img src="imagenes/pf_blank.gif" data-original="imagenes/portfolio/pf%20(1).jpg" alt="" />
-                            </div>
-                        </div>
-                        <!-- close gallery item -->
-
-                        <!-- gallery item -->
-                        <div class="span4  item">
-                            <div class="picframe">
-                                <span class="overlay">
-                                    <span class="info-area">
-                                        <a class="img-icon-url" href="project-single.html"></a>
-                                        <a class="img-icon-zoom" href="imagenes/portfolio/pf%20(1).jpg" data-type="prettyPhoto[gallery]" title=""></a>
-                                    </span>
-                                    <span class="pf_text">
-                                        <span class="project-name">Project Name #1</span>
-                                        <span>Mobile</span>
-                                    </span>
-                                </span>
-                                <img src="imagenes/pf_blank.gif" data-original="imagenes/portfolio/pf%20(1).jpg" alt="" />
-                            </div>
-                        </div>
-                        <!-- close gallery item -->
-
-                        <!-- gallery item -->
-                        <div class="span4  item">
-                            <div class="picframe">
-                                <span class="overlay">
-                                    <span class="info-area">
-                                        <a class="img-icon-url" href="project-single.html"></a>
-                                        <a class="img-icon-zoom" href="imagenes/portfolio/pf%20(1).jpg" data-type="prettyPhoto[gallery]" title=""></a>
-                                    </span>
-                                    <span class="pf_text">
-                                        <span class="project-name">Project Name #1</span>
-                                        <span>Mobile</span>
-                                    </span>
-                                </span>
-                                <img src="imagenes/pf_blank.gif" data-original="imagenes/portfolio/pf%20(1).jpg" alt="" />
-                            </div>
-                        </div>
-                        <!-- close gallery item -->
-
-                        <!-- gallery item -->
-                        <div class="span4  item">
-                            <div class="picframe">
-                                <span class="overlay">
-                                    <span class="info-area">
-                                        <a class="img-icon-url" href="project-single.html"></a>
-                                        <a class="img-icon-zoom" href="imagenes/portfolio/pf%20(1).jpg" data-type="prettyPhoto[gallery]" title=""></a>
-                                    </span>
-                                    <span class="pf_text">
-                                        <span class="project-name">Project Name #1</span>
-                                        <span>Mobile</span>
-                                    </span>
-                                </span>
-                                <img src="imagenes/pf_blank.gif" data-original="imagenes/portfolio/pf%20(1).jpg" alt="" />
-                            </div>
-                        </div>
-                        <!-- close gallery item -->
-
-                        <!-- gallery item -->
-                        <div class="span4  item">
-                            <div class="picframe">
-                                <span class="overlay">
-                                    <span class="info-area">
-                                        <a class="img-icon-url" href="project-single.html"></a>
-                                        <a class="img-icon-zoom" href="imagenes/portfolio/pf%20(1).jpg" data-type="prettyPhoto[gallery]" title=""></a>
-                                    </span>
-                                    <span class="pf_text">
-                                        <span class="project-name">Project Name #1</span>
-                                        <span>Mobile</span>
-                                    </span>
-                                </span>
-                                <img src="imagenes/pf_blank.gif" data-original="imagenes/portfolio/pf%20(1).jpg" alt="" />
-                            </div>
-                        </div>
-                        <!-- close gallery item -->
-
-                        <!-- gallery item -->
-                        <div class="span4  item">
-                            <div class="picframe">
-                                <span class="overlay">
-                                    <span class="info-area">
-                                        <a class="img-icon-url" href="project-single.html"></a>
-                                        <a class="img-icon-zoom" href="imagenes/portfolio/pf%20(1).jpg" data-type="prettyPhoto[gallery]" title=""></a>
-                                    </span>
-                                    <span class="pf_text">
-                                        <span class="project-name">Project Name #1</span>
-                                        <span>Mobile</span>
-                                    </span>
-                                </span>
-                                <img src="imagenes/pf_blank.gif" data-original="imagenes/portfolio/pf%20(1).jpg" alt="" />
-                            </div>
-                        </div>
-                        <!-- close gallery item -->
-
-                        <!-- gallery item -->
-                        <div class="span4  item">
-                            <div class="picframe">
-                                <span class="overlay">
-                                    <span class="info-area">
-                                        <a class="img-icon-url" href="project-single.html"></a>
-                                        <a class="img-icon-zoom" href="imagenes/portfolio/pf%20(1).jpg" data-type="prettyPhoto[gallery]" title=""></a>
-                                    </span>
-                                    <span class="pf_text">
-                                        <span class="project-name">Project Name #1</span>
-                                        <span>Mobile</span>
-                                    </span>
-                                </span>
-                                <img src="imagenes/pf_blank.gif" data-original="imagenes/portfolio/pf%20(1).jpg" alt="" />
-                            </div>
-                        </div>
-                        <!-- close gallery item -->
-
-                        <!-- gallery item -->
-                        <div class="span4  item">
-                            <div class="picframe">
-                                <span class="overlay">
-                                    <span class="info-area">
-                                        <a class="img-icon-url" href="project-single.html"></a>
-                                        <a class="img-icon-zoom" href="imagenes/portfolio/pf%20(1).jpg" data-type="prettyPhoto[gallery]" title=""></a>
-                                    </span>
-                                    <span class="pf_text">
-                                        <span class="project-name">Project Name #1</span>
-                                        <span>Mobile</span>
-                                    </span>
-                                </span>
-                                <img src="imagenes/pf_blank.gif" data-original="imagenes/portfolio/pf%20(1).jpg" alt="" />
-                            </div>
-                        </div>
-                        <!-- close gallery item -->
-
-                        <!-- gallery item -->
-                        <div class="span4  item">
-                            <div class="picframe">
-                                <span class="overlay">
-                                    <span class="info-area">
-                                        <a class="img-icon-url" href="project-single.html"></a>
-                                        <a class="img-icon-zoom" href="imagenes/portfolio/pf%20(1).jpg" data-type="prettyPhoto[gallery]" title=""></a>
-                                    </span>
-                                    <span class="pf_text">
-                                        <span class="project-name">Project Name #1</span>
-                                        <span>Mobile</span>
-                                    </span>
-                                </span>
-                                <img src="imagenes/pf_blank.gif" data-original="imagenes/portfolio/pf%20(1).jpg" alt="" />
-                            </div>
-                        </div>
-                        <!-- close gallery item -->
+                        <?php } ?>                        
 
                     </div>
                 </div>
@@ -266,91 +82,12 @@ require_once("panel@marost/conexion/funciones.php");
         <!-- content close -->
 
         <!-- footer begin -->
-        <footer>
-            <div class="container">
-                <div class="row">
-                    <div class="span3">
-                        <img src="imagenes/logo-small.png" alt="logo"><br>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                    </div>
-
-                    <div class="span3">
-                        <div class="widget widget_recent_post">
-                            <h3>Latest News</h3>
-                            <ul>
-                                <li><a href="#">Sed ut perspiciatis unde omnis</a></li>
-                                <li><a href="#">Duis aute irure dolor in</a></li>
-                                <li><a href="#">Excepteur sint occaecat cupidatat </a></li>
-                                <li><a href="#">Nemo enim ipsam voluptatem quia  </a></li>
-                                <li><a href="#">Quis autem vel eum iure</a></li>
-                            </ul>
-                        </div>
-                    </div>
-
-                    <div class="span3">
-                        <div class="widget widget_tags">
-                            <h3>Tags</h3>
-                            <ul>
-                                <li><a href="#link">Art</a></li>
-                                <li><a href="#link">Application</a></li>
-                                <li><a href="#link">Design</a></li>
-                                <li><a href="#link">Entertainment</a></li>
-                                <li><a href="#link">Internet</a></li>
-                                <li><a href="#link">Marketing</a></li>
-                                <li><a href="#link">Music</a></li>
-                                <li><a href="#link">Print</a></li>
-                                <li><a href="#link">Programming</a></li>
-                            </ul>
-                        </div>
-                    </div>
-
-                    <div class="span3">
-                        <h3>Contact Us</h3>
-                        <div class="widget widget-address">
-                            <address>
-                                100 Mainstreet Center, Sydney
-                                <span><strong>Phone:</strong>(208) 333 9296</span>
-                                <span><strong>Fax:</strong>(208) 333 9298</span>
-                                <span><strong>Email:</strong><a href="mailto:contact@example.com">contact@example.com</a></span>
-                                <span><strong>Web:</strong><a href="#test">http://example.com</a></span>
-                            </address>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="subfooter">
-                <div class="container">
-                    <div class="row">
-                        <div class="span6">
-                            &copy; Copyright 2013 - Truehost by Designesia                     
-                        </div>
-                        <div class="span6">
-                            <nav>
-                                <ul>
-                                    <li><a href="#">Home</a></li>
-                                    <li><a href="#">Features</a></li>
-                                    <li><a href="#">Blog</a></li>
-                                    <li><a href="#">About</a></li>
-                                    <li><a href="#">Services</a></li>
-                                    <li><a href="#">Contact</a></li>
-                                </ul>
-                            </nav>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-        </footer>
+        <?php require_once("wg-footer.php"); ?>
         <!-- footer close -->
 
     </div>
 
-    <!-- style switcher
-    ================================================== -->
-    <script src="js/switcher.js"></script>
-    
-
+<?php require_once("wg-footer-script.php"); ?>
 
 </body>
 </html>
