d584191f4dbc6b19d6548d0ac1ee04f858373e35
Marco Lopez <mlopez@marostdevelopers.com>
2014-08-27 16:53:35 -0500

    eliminacion de carpetas CkEditor CkFinder PlUpload

--- panel@marost/js/plugins/ckeditor/config.js
+++ panel@marost/js/plugins/ckeditor/config.js
@@ -4,10 +4,10 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 */
 
 CKEDITOR.editorConfig = function(config) {
-   config.filebrowserBrowseUrl = '/panel@pmarketing/js/plugins/kcfinder/browse.php?type=files';
-   config.filebrowserImageBrowseUrl = '/panel@pmarketing/js/plugins/kcfinder/browse.php?type=images';
-   config.filebrowserFlashBrowseUrl = '/panel@pmarketing/js/plugins/kcfinder/browse.php?type=flash';
-   config.filebrowserUploadUrl = '/panel@pmarketing/js/plugins/kcfinder/upload.php?type=files';
-   config.filebrowserImageUploadUrl = '/panel@pmarketing/js/plugins/kcfinder/upload.php?type=images';
-   config.filebrowserFlashUploadUrl = '/panel@pmarketing/js/plugins/kcfinder/upload.php?type=flash';
+   config.filebrowserBrowseUrl = '/panel@marost/js/plugins/kcfinder/browse.php?type=files';
+   config.filebrowserImageBrowseUrl = '/panel@marost/js/plugins/kcfinder/browse.php?type=images';
+   config.filebrowserFlashBrowseUrl = '/panel@marost/js/plugins/kcfinder/browse.php?type=flash';
+   config.filebrowserUploadUrl = '/panel@marost/js/plugins/kcfinder/upload.php?type=files';
+   config.filebrowserImageUploadUrl = '/panel@marost/js/plugins/kcfinder/upload.php?type=images';
+   config.filebrowserFlashUploadUrl = '/panel@marost/js/plugins/kcfinder/upload.php?type=flash';
 };
