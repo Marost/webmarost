2d562efb0994e0798f9e2f54820724d610459bf4
Marco Lopez <mlopez@marostdevelopers.com>
2014-09-01 13:05:14 -0500

    mostrar categorias, tags de noticia y todos los tags en sidebar

--- w-sidebar.php
+++ w-sidebar.php
@@ -1,6 +1,14 @@
+<?php
+//CATEGORIAS
+$rst_CatSb=mysql_query("SELECT * FROM mrt_noticia_categoria ORDER BY categoria ASC;", $conexion);
+
+//TAGS
+$rst_TagSb=mysql_query("SELECT * FROM mrt_noticia_tags ORDER BY nombre ASC;", $conexion);
+?>
 <!-- right sidebar starts -->
 <div class="right_sidebar">
 
+    <!--
     <div class="site-search-area">
 
         <form method="get" id="site-searchform" action="blog.html">
@@ -10,31 +18,42 @@
             </div>
         </form>
 
-    </div><!-- end site search -->
+    </div><!-- end site search --
 
     <div class="clearfix mar_top4"></div>
+    -->
 
     <div class="sidebar_widget">
 
-        <div class="sidebar_title"><h3>Site <i>Categories</i></h3></div>
+        <div class="sidebar_title"><h3>Categorias</h3></div>
         <ul class="arrows_list1">
-            <li><a href="#"><i class="fa fa-angle-right"></i> Website Design</a></li>
-            <li><a href="#"><i class="fa fa-angle-right"></i> Make Money Online</a></li>
-            <li><a href="#"><i class="fa fa-angle-right"></i> Photography</a></li>
-            <li><a href="#"><i class="fa fa-angle-right"></i> Wordpress Themes</a></li>
-            <li><a href="#"><i class="fa fa-angle-right"></i> Learn Web Tutorials</a></li>
+            <?php while($fila_CatSb=mysql_fetch_array($rst_CatSb)){
+                    $CatSb_id=$fila_CatSb["id"];
+                    $CatSb_url=$fila_CatSb["url"];
+                    $CatSb_titulo=$fila_CatSb["categoria"];
+
+                    //URL
+                    $CatSb_UrlWeb=$web."categoria/".$CatSb_url;
+            ?>
+            <li>
+                <a href="<?php echo $CatSb_UrlWeb; ?>">
+                    <i class="fa fa-angle-right"></i> <?php echo $CatSb_titulo; ?>
+                </a>
+            </li>
+            <?php } ?>
         </ul>
 
     </div><!-- end section -->
 
     <div class="clearfix mar_top4"></div>
 
+    <?php if($w_tagsNota==true){ ?>
     <div class="sidebar_widget">
 
         <div id="tabs">
 
             <ul class="tabs">
-                <li class="last"><a href="javascript();">Tags</a></li>
+                <li class="last" style="width: 100%;"><a href="javascript:;">Tags de la noticia</a></li>
             </ul><!-- /# end tab links -->
 
             <div class="tab_container">
@@ -42,13 +61,19 @@
                 <div id="tab3" class="tab_content">
                     <ul class="tags">
 
-                        <li><a href="#">2013</a></li>
-                        <li><a href="#"><b>Amazing</b></a></li>
-                        <li><a href="#">Animation</a></li>
-                        <li><a href="#">Beautiful</a></li>
-                        <li><a href="#"><b>Cartoon</b></a></li>
-                        <li><a href="#">Comedy</a></li>
-                        <li><a href="#"><b>Cool</b></a></li>
+                        <?php while($fila_tagsNota=mysql_fetch_array($rst_tagsNota)){
+                            $tagsNota_id=$fila_tagsNota["id"];
+                            $tagsNota_url=$fila_tagsNota["url"];
+                            $tagsNota_nombre=$fila_tagsNota["nombre"];
+
+                            //URL
+                            $tagsNota_WebURL=$web."tags/".$tagsNota_id."-".$tagsNota_url;
+                            if(in_array($tagsNota_id, $tags)){
+                        ?>
+                            <li>
+                                <a href="<?php echo $tagsNota_WebURL; ?>"><?php echo $tagsNota_nombre; ?></a>
+                            </li>
+                        <?php }} ?>
 
                     </ul>
                 </div>
@@ -59,20 +84,47 @@
 
     </div><!-- end section -->
 
-    <div class="clearfix mar_top5"></div>
+    <div class="clearfix mar_top4"></div>
 
+    <?php } ?>
+
+    <?php if($w_tagsBlog==true){ ?>
     <div class="sidebar_widget">
 
-        <div class="sidebar_title"><h3>Site <i>Archives</i></h3></div>
+        <div id="tabs">
 
-        <ul class="arrows_list1">
-            <li><a href="#"><i class="fa fa-angle-right"></i> November 2013</a></li>
-            <li><a href="#"><i class="fa fa-angle-right"></i> October 2013</a></li>
-            <li><a href="#"><i class="fa fa-angle-right"></i> September 2013</a></li>
-            <li><a href="#"><i class="fa fa-angle-right"></i> August 2013</a></li>
-            <li><a href="#"><i class="fa fa-angle-right"></i> July 2013</a></li>
-        </ul>
+            <ul class="tabs">
+                <li class="last"><a href="javascript:;">Tags</a></li>
+            </ul><!-- /# end tab links -->
+
+            <div class="tab_container">
+
+                <div id="tab3" class="tab_content">
+                    <ul class="tags">
+
+                        <?php while($fila_TagSb=mysql_fetch_array($rst_TagSb)){
+                            $TagSb_id=$fila_TagSb["id"];
+                            $TagSb_url=$fila_TagSb["url"];
+                            $TagSb_titulo=$fila_TagSb["nombre"];
+
+                            //URL
+                            $TagSb_UrlWeb=$web."tags/".$TagSb_id."-".$TagSb_url;
+                            ?>
+                            <li>
+                                <a href="<?php echo $TagSb_UrlWeb; ?>">
+                                    <?php echo $TagSb_titulo; ?>
+                                </a>
+                            </li>
+                        <?php } ?>
+
+                    </ul>
+                </div>
+
+            </div>
+
+        </div>
 
     </div><!-- end section -->
+    <?php } ?>
 
 </div>
