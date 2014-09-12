0f377576829f4c06e39ee06cba0c48da3a7326f7
Marco Lopez <mlopez@marostdevelopers.com>
2014-09-12 11:06:53 -0500

    cambio de texto en linea inferior de footer

--- w-footer-texto.php
+++ w-footer-texto.php
@@ -3,7 +3,7 @@
     <div class="waves_graph2"></div>
 
     <div class="big_text2">
-        <div class="container"><i>Lo mejor en Servicios de Hosting para pequeñas empresas.</i> ¡TU NEGOCIO ES NUESTRO TRABAJO!</div>
+        <div class="container"><i>Lo mejor en Servicios Informáticos para pequeñas empresas.</i> ¡TU NEGOCIO ES NUESTRO TRABAJO!</div>
     </div>
 
 </div><!-- end bottom section -->
